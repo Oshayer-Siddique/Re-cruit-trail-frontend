b18dd9a161da537e88e1f8ea924679e60b788653
Oshayer Siddique <oshayer.siddique2001@gmail.com>
2024-03-19 15:46:21 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import About from './Components/About';
+import Alert from './Components/Alert';
+import React, { useState } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+let name = "Oshayer"
 
 function App() {
+
+  const [mode, setMode] = useState('dark');
+
+  const toggleMode = () => {
+    if (mode == 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#0a335c';
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+    <Router>
+      <div>
+        <Navbar title="TEXTDATA" about="ABOUT US" mode={mode} togglemode={toggleMode} />
+        <Alert />
+        <div className="container">
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <TextForm email="EMAIL ADDRESS" mode={mode} />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+
+
+
+
+
   );
 }
+
+
 
 export default App;
---
+++ src/Components/TextForm.js
@@ -0,0 +1,67 @@
+import React, {useState} from 'react'
+
+
+
+export default function TextForm(props) {
+    const handleUpclick  = ()=>{
+        console.log("Upper case is cliccked" + text);
+        let newtext = text.toUpperCase();
+        setText(newtext);
+
+    }
+
+    const handleOnchange = (event)=>{
+        console.log("On change");
+        setText(event.target.value);
+
+    }
+
+
+    const [text, setText] = useState('Enter the Text Oshayer');
+    
+    return (
+        <>
+        <div className='container' style={{color: props.mood == '#0a335c'?'white':'black'}}>
+            <form>
+                <div className="form-group" style={{backgroundColor: props.mood == '#0a335c'?'white':'dark'}}  >
+                    <label for="exampleFormControlInput1"><h2>{props.email}</h2></label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                </div>
+                <div className="form-group">
+                    <label for="exampleFormControlSelect1">Example select</label>
+                    <select className="form-control" id="exampleFormControlSelect1">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label for="exampleFormControlSelect2">Example multiple select</label>
+                    <select multiple className="form-control" id="exampleFormControlSelect2">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label for="exampleFormControlTextarea1">Enter DATA</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={text} onChange={handleOnchange}></textarea>
+                </div>
+            </form>
+            <button className='btn btn-primary' onClick={handleUpclick}>Convert to UpperCase</button>
+
+        </div>
+        <div className="container my-3">
+            <h3>Your text summary</h3>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+
+        </div>
+        </>
+
+        
+    )
+}
