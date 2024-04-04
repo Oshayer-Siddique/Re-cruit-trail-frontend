a94f58723ce73a8faebc15a602d78c7f6eb036f5
Oshayer-Siddique <65402474+Oshayer-Siddique@users.noreply.github.com>
2024-04-04 21:25:47 +0600

    new style main

--- src/Components/ConversationFile.js
+++ src/Components/ConversationFile.js
@@ -7,7 +7,7 @@ const ConversationFile = () => {
 
   const fetchConversation = () => {
     setLoading(true);
-    fetch('http://localhost:5000/conversationfile')
+    fetch('https://re-cruit-trial-backend.onrender.com/conversationfile')
       .then(response => response.json())
       .then(data => {
         setConversationData(data.conversation);
@@ -46,7 +46,7 @@ const ConversationFile = () => {
 
       {loading && <p>Loading...</p>}
       {conversationData && !loading && (
-        <div className="conversation-box">
+        <div className="conversation-box1">
           {conversationData.map((message, index) => (
             <div key={index} id={`message-${index}`} className="message">
               <p className="timestamp">{message.timestamp}</p>
